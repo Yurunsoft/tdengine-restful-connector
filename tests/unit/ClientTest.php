a45749839af06a86b5c7a91d7bbb0630995206db
Yurun <admin@yurunsoft.com>
2021-12-08 13:36:02 +0800

    新增 TDEngineManager

--- tests/unit/ClientTest.php
+++ tests/unit/ClientTest.php
@@ -13,15 +13,7 @@ class ClientTest extends TestCase
 {
     private function getClient(): Client
     {
-        return new Client(new ClientConfig([
-            'host'            => getenv('TDENGINE_HOST') ?: '127.0.0.1',
-            'hostName'        => getenv('TDENGINE_HOST_NAME') ?: '',
-            'port'            => getenv('TDENGINE_PORT') ?: 6041,
-            'user'            => getenv('TDENGINE_USER') ?: 'root',
-            'password'        => getenv('TDENGINE_PASSWORD') ?: 'taosdata',
-            'ssl'             => getenv('TDENGINE_SSL') ?: false,
-            'timestampFormat' => getenv('TDENGINE_TIMESTAMP_FORMAT') ?: TimeStampFormat::LOCAL_STRING,
-        ]));
+        return new Client(TestUtil::getClientConfig());
     }
 
     private function getDbName(): string
---
+++ tests/unit/ManagerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Yurun\TDEngine\Test;
+
+use PHPUnit\Framework\TestCase;
+use Yurun\TDEngine\ClientConfig;
+use Yurun\TDEngine\TDEngineManager;
+
+class ConnectionTest extends TestCase
+{
+    public function testClientConfig(): void
+    {
+        $this->assertNull(TDEngineManager::getClientConfig());
+
+        TDEngineManager::setClientConfig('test', TestUtil::getClientConfig());
+        TDEngineManager::setClientConfig('test2', TestUtil::getClientConfig());
+
+        $this->assertInstanceOf(ClientConfig::class, TDEngineManager::getClientConfig('test'));
+    }
+
+    public function testRemoveClientConfig(): void
+    {
+        TDEngineManager::setClientConfig('testx', new ClientConfig);
+        $this->assertNotNull(TDEngineManager::getClientConfig('testx'));
+        TDEngineManager::removeClientConfig('testx');
+        $this->assertNull(TDEngineManager::getClientConfig('testx'));
+    }
+
+    public function testDefaultClientName(): void
+    {
+        $this->assertNull(TDEngineManager::getDefaultClientName());
+        TDEngineManager::setDefaultClientName('test');
+        $this->assertEquals('test', TDEngineManager::getDefaultClientName());
+    }
+
+    public function testGetClient(): void
+    {
+        $client = TDEngineManager::getClient();
+        $this->assertNotNull($client);
+    }
+
+}
