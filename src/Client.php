3b68c4de880e9a8924e734285283aa40fafc2173
Yurun <admin@yurunsoft.com>
2022-11-25 13:29:22 +0800

    修复 Swoole 下启用 keepAlive 并发请求

--- src/Client.php
+++ src/Client.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Yurun\TDEngine;
 
 use InvalidArgumentException;
+use Swoole\Coroutine;
 use Yurun\TDEngine\Action\Sql\SqlResult;
 use Yurun\TDEngine\Constants\TimeStampFormat;
 use Yurun\TDEngine\Exception\NetworkException;
 use Yurun\TDEngine\Exception\OperationException;
 use Yurun\Util\HttpRequest;
+use Yurun\Util\YurunHttp\Http\Psr7\Request;
 use Yurun\Util\YurunHttp\Http\Psr7\Uri;
 
 class Client
@@ -24,13 +26,15 @@ class Client
      */
     protected $httpClient;
 
+    /**
+     * @var bool
+     */
+    protected $haveSwoole;
+
     public function __construct(ClientConfig $config)
     {
         $this->config = $config;
-        if ($config->getKeepAlive())
-        {
-            $this->httpClient = new HttpRequest();
-        }
+        $this->haveSwoole = \defined('SWOOLE_VERSION');
     }
 
     public function getConfig(): ClientConfig
@@ -45,17 +49,35 @@ class Client
         return Uri::makeUri($config->getHost(), $path, '', $config->getPort(), $config->getSsl() ? 'https' : 'http', '', $config->getUser() . ':' . $config->getPassword())->__toString();
     }
 
-    public function request(string $path, string $postBody): array
+    private function getHttpClient(): HttpRequest
     {
-        $config = $this->getConfig();
-        if ($config->getKeepAlive())
+        if ($this->getConfig()->getKeepAlive() && $this->haveSwoole && Coroutine::getCid() > 0)
         {
-            $httpClient = $this->httpClient;
+            $context = Coroutine::getContext();
+            $objectHash = spl_object_hash($this);
+            if (isset($context[self::class][$objectHash]))
+            {
+                return $context[self::class][$objectHash];
+            }
+            else
+            {
+                return $context[self::class][$objectHash] = new HttpRequest();
+            }
+        }
+        elseif (isset($this->httpClient))
+        {
+            return $this->httpClient;
         }
         else
         {
-            $httpClient = new HttpRequest();
+            return new HttpRequest();
         }
+    }
+
+    public function request(string $path, string $postBody): array
+    {
+        $config = $this->getConfig();
+        $httpClient = $this->getHttpClient();
         if ('' !== ($hostName = $config->getHostName()))
         {
             $httpClient->header('Host', $hostName);
